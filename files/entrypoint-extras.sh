c01c13d80b30095b5afcb41ac552ead98acb0f84
Ernesto Baschny <eb@cron.eu>
2025-07-02 19:54:50 +0200

    Keep special handling of XDEBUG_MOD

--- files/entrypoint-extras.sh
+++ files/entrypoint-extras.sh
@@ -5,15 +5,6 @@
 #
 # Mainly tweaking php settings based on ENV variables
 
-# Really disable XDEBUG if not required
-if [ -z "${XDEBUG_MODE}" ] || [ "${XDEBUG_MODE}" = "off" ]; then
-  # completely not load xdebug if its off
-  rm -f /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
-else
-  echo "* Enabling XDEBUG: $XDEBUG_MODE"
-  echo "zend_extension=xdebug.so" > /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
-fi
-
 # Controls which extensions are enabled.
 
 if [ ! -z "${PHP_EXTENSIONS}" ]; then
@@ -64,6 +55,17 @@ else
       fi
     done
   fi
+fi
+
+# Special handling for XDEBUG through XDEBUG_MODE:
+
+# Really disable XDEBUG if not required
+if [ -z "${XDEBUG_MODE}" ] || [ "${XDEBUG_MODE}" = "off" ]; then
+  # completely not load xdebug if its off
+  rm -f /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+else
+  echo "* Enabling XDEBUG: $XDEBUG_MODE"
+  echo "zend_extension=xdebug.so" > /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 fi
 
 if [ ! -z "${APPLICATION_UID}" ]; then
