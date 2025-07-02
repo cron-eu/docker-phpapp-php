cron-eu/docker-phpapp-php

commit 192d55f36e202be85fc64fe518fedc3bb666d0c0

Get rid of PHP_PACKAGES arg to allow dynamic changing the list in the Dockerfile

Dockerfile
@@ -4,41 +4,12 @@
 
 ARG PHP_MINOR_VERSION=8.2
 ARG NODE_VERSION=18
-ARG PHP_PACKAGES=" \
-    apcu \
-    bcmath \
-    bz2 \
-    calendar \
-    exif \
-    gd \
-    gettext \
-    igbinary \
-    imagick \
-    intl \
-    mcrypt \
-    mysqli \
-    opcache \
-    pcntl \
-    pdo_mysql \
-    redis \
-    shmop \
-    soap \
-    sockets \
-    sysvmsg \
-    sysvsem \
-    sysvshm \
-    uuid \
-    xdebug \
-    yaml \
-    zip \
-"
 
 # -------------------------------------------------------------------------
 
 FROM php:${PHP_MINOR_VERSION}-fpm as php-fpm
 
 ARG PHP_MINOR_VERSION
-ARG PHP_PACKAGES
 
 RUN echo 'APT::Install-Recommends "0";' >> /etc/apt/apt.conf.d/phpapp-norecommends && \
     echo 'APT::Install-Suggests "0";' >> /etc/apt/apt.conf.d/phpapp-suggests
@@ -59,7 +30,36 @@ RUN <<EOF
 EOF
 
 COPY --from=mlocati/php-extension-installer /usr/bin/install-php-extensions /usr/bin/
-RUN install-php-extensions $PHP_PACKAGES
+RUN <<EOT bash
+    set -ex
+    PHP_EXTENSIONS=(
+      "bz2"
+      "calendar"
+      "exif"
+      "gd"
+      "gettext"
+      "igbinary"
+      "imagick"
+      "intl"
+      "mcrypt"
+      "mysqli"
+      "opcache"
+      "pcntl"
+      "pdo_mysql"
+      "redis"
+      "shmop"
+      "soap"
+      "sockets"
+      "sysvmsg"
+      "sysvsem"
+      "sysvshm"
+      "uuid"
+      "xdebug"
+      "yaml"
+      "zip"
+    )
+    install-php-extensions \${PHP_EXTENSIONS[@]}
+EOT
 
 # Other tools
 RUN apt-get -qq update && apt-get -q install -y \

commit 88938285261bab72494add05542f8b9a972ca841

Readd removed extensions (apcu, bcmath)
Regression from https://github.com/cron-eu/docker-phpapp-php/pull/42/commits/192d55f36e202be85fc64fe518fedc3bb666d0c0

Dockerfile
@@ -33,6 +33,8 @@ COPY --from=mlocati/php-extension-installer /usr/bin/install-php-extensions /usr
 RUN <<EOT bash
     set -ex
     PHP_EXTENSIONS=(
+      "apcu"
+      "bcmath"
       "bz2"
       "calendar"
       "exif"